8ea172abb990cc7da2e68a7890c33bb0864e97df
Jahija303 <57103143+Jahija303@users.noreply.github.com>
2021-06-15 02:23:50 +0200

    minor edit

--- seeders/employeeSeeder.js
+++ seeders/employeeSeeder.js
@@ -2,10 +2,10 @@
 const Employee = require('../models/employee.js');
 
 //assign dummy data to employee models 
-const Employee1 = new Employee(1,'Jahija','Okan',1,false);
-const Employee2 = new Employee(2,'Mehmed','Mehmedovski',2,true);
-const Employee3 = new Employee(3,'Salih','Selimovic',0,true);
-const Employee4 = new Employee(4,'Kerim','Muratbegovic',1,false);
+const Employee1 = new Employee(1,'Jahija','Okan',1,true);
+const Employee2 = new Employee(2,'John','Smith',2,false);
+const Employee3 = new Employee(3,'Morgan','Freeman',0,true);
+const Employee4 = new Employee(4,'Leonardno','DiCaprio',1,false);
 
 //create an empty array where employees will be stored
 const employees = [];
